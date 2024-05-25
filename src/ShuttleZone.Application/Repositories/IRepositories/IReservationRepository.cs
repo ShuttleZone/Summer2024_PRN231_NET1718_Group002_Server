e10f434fa5ae2f85ca68908cf8c4c896ed71dfbd
supernhilty <nhilty302814@gmail.com>
2024-05-25 15:23:56 +0700

    [AddDAL][Nhi] update solution

--- src/ShuttleZone.Application/Repositories/IRepositories/IReservationRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShuttleZone.Application.Common.Interfaces;
-using ShuttleZone.Domain.Entities;
-
-namespace ShuttleZone.Application.Repositories.IRepositories
-{
-    public interface IReservationRepository: IGenericRepository<Reservation>
-    {
-    }
-}
