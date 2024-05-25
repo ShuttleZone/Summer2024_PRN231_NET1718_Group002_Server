c7393b9e44038a59c1de0be4a3b5245be7521bf9
supernhilty <nhilty302814@gmail.com>
2024-05-25 11:44:13 +0700

    [Repository][Nhi] add generic repo implementation

---
+++ src/ShuttleZone.Application/Repositories/ReservationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShuttleZone.Application.Common.Implementations;
+using ShuttleZone.Application.Repositories.IRepositories;
+using ShuttleZone.Domain.Entities;
+
+namespace ShuttleZone.Application.Repositories
+{
+    public class ReservationRepository : GenericRepository<Reservation>, IReservationRepository
+    {
+        public ReservationRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
