aa7f83bf0b134988918227bfe02a4e8aab9c9371
supernhilty <nhilty302814@gmail.com>
2024-05-27 11:30:39 +0700

    [MyReservationsList][Nhi] add my-reservations endpoint

---
+++ src/ShuttleZone.Api/Controllers/ReservationDetailsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using ShuttleZone.Api.Controllers.BaseControllers;
+using ShuttleZone.Application.Services.IServices;
+
+namespace ShuttleZone.Api.Controllers
+{
+    public class ReservationDetailsController : BaseApiController
+    {
+        private readonly IReservationService _reservationService;
+
+        public ReservationDetailsController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [EnableQuery]
+        [HttpGet("my-reservations")]
+        public IActionResult GetMyReservationDetails()
+        {
+            //placeholder for logined user
+            var userId = new Guid("26A7CC4E-3F9B-4923-809E-2F9B771D994F");
+
+            var response = _reservationService.GetMyReservationDetails(userId);
+
+            return Ok(response);
+        }
+    }
+}
--- src/ShuttleZone.Application/Services/ReservationService.cs
+++ src/ShuttleZone.Application/Services/ReservationService.cs
@@ -1,19 +1,32 @@
-﻿using ShuttleZone.Application.Services.IServices;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using ShuttleZone.Application.Services.IServices;
 using ShuttleZone.DAL.Common.Interfaces;
+using ShuttleZone.Domain.WebResponses.ReservationDetails;
 
 namespace ShuttleZone.Application.Services
 {
     public class ReservationService : IReservationService
     {
         private readonly IUnitOfWork  _unitOfWork;
-        public ReservationService(IUnitOfWork unitOfWork)
+        private readonly IMapper _mapper;
+        public ReservationService(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
-        /*public Task<ReservationDetailsResponse> GetMyReservationDetails(Guid currentUser)
+        public IQueryable<ReservationDetailsResponse> GetMyReservationDetails(Guid currentUser)
         {
-            //_unitOfWork.ReservationRepository.Get
-            return;
-        }*/
+            var reservationDetailsQuery = _unitOfWork.ReservationRepository.GetAll()
+                .Where(r => r.CustomerId == currentUser)
+                .Include(r => r.ReservationDetails)
+                .SelectMany(r => r.ReservationDetails);
+
+            var reservationDetailsResponse = reservationDetailsQuery
+                .ProjectTo<ReservationDetailsResponse>(_mapper.ConfigurationProvider);
+
+            return reservationDetailsResponse;
+        }
     }
 }
