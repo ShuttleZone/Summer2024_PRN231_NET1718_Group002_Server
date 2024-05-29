9fbd7e9aeb04cb714a5370fb6f21f229d7048f1d
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-05-29 15:20:08 +0700

    [CourtBooking][Hai] add dto for list response

--- src/ShuttleZone.Domain/WebResponses/Club/DtoClubResponse.cs
+++ src/ShuttleZone.Domain/WebResponses/Club/DtoClubResponse.cs
@@ -9,9 +9,10 @@ public record DtoClubResponse
     public required string ClubAddress { get; set; }
     public required string ClubPhone { get; set; }
     public required string ClubDescription { get; set; }
+    public double MinDuration { get; set; }
     public TimeOnly OpenTime { get; set; }
     public TimeOnly CloseTime { get; set; }
-
+    public ICollection<DtoCourt> Courts { get; set; }= new List<DtoCourt>();
     public ICollection<DtoReviewResponse> Reviews { get; set; } = new List<DtoReviewResponse>();
     public ICollection<DtoClubImageResponse> ClubImages { get; set; } = new List<DtoClubImageResponse>();
 }
@@ -26,4 +27,12 @@ public record DtoClubImageResponse
 {
     public Guid Id { get; set; }
     public required string ImageUrl { get; set; }
+}
+
+public record DtoCourt
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public CourtType CourtType { get; set; }
+    public CourtStatus CourtStatus { get; set; }
 }
