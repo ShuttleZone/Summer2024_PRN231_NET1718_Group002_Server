5aa7ae3a59874bb36d7033be8510eac36e367e20
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2024-05-31 14:18:42 +0700

    [CourtList][Hai] define dtoentity

--- src/ShuttleZone.Application/AutoMapper/CourtMappingProfile.cs
+++ src/ShuttleZone.Application/AutoMapper/CourtMappingProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Xml;
+using AutoMapper;
 using ShuttleZone.Domain.Entities;
 using ShuttleZone.Domain.WebResponses.Court;
 
@@ -9,6 +10,9 @@ public class CourtMappingProfile : Profile
     public CourtMappingProfile()
     {
         CreateMap<Court, DtoCourtResponse>()
-            .ForMember(x => x.ClubName, opt => opt.MapFrom(x => x.Club.ClubName));
+            .ForMember(x => x.ClubName, opt => opt.MapFrom(x => x.Club.ClubName))
+            .ForMember(dto => dto.OpenTime, opt => opt.MapFrom(x => x.Club.OpenTime))
+            .ForMember(dto => dto.CloseTime, opt => opt.MapFrom(x => x.Club.CloseTime))
+            .ForMember(dto => dto.MinDuration, opt => opt.MapFrom(x => x.Club.MinDuration));    
     }
 }
--- src/ShuttleZone.Domain/WebResponses/Court/DtoCourtResponse.cs
+++ src/ShuttleZone.Domain/WebResponses/Court/DtoCourtResponse.cs
@@ -5,9 +5,13 @@ namespace ShuttleZone.Domain.WebResponses.Court;
 
 public class DtoCourtResponse
 {
+    public Guid Id { get; set; }
     public string? Name { get; set; }
     public CourtType CourtType { get; set; }
     public CourtStatus CourtStatus { get; set; }
     public required Guid ClubId { get; set; }
     public required string ClubName { get; set; }
+    public TimeOnly OpenTime { get; set; }
+    public TimeOnly CloseTime { get; set; }
+    public double MinDuration { get; set; }
 }
