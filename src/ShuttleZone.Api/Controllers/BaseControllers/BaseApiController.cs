199b584dd8277e6237cfbcbda94dd49f068ffa22
duykasama <duykasama@gmail.com>
2024-06-11 13:16:29 +0700

    [CreateContest][Duy] implement create contest logic

--- src/ShuttleZone.Api/Controllers/BaseControllers/BaseApiController.cs
+++ src/ShuttleZone.Api/Controllers/BaseControllers/BaseApiController.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using ShuttleZone.Common.Exceptions;
 
 namespace ShuttleZone.Api.Controllers.BaseControllers
 {
     public abstract class BaseApiController : ODataController
     {
+        /// <summary>
+        /// Handles the result of an HTTP action without a return value.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>The result of the action.</returns>
+        /// <remarks>
+        /// If the model state is not valid, the method will return a bad request with the model state.
+        /// If the action throws an exception, the method will return a status code of 500 with the exception message.
+        /// </remarks>
+        protected IActionResult HandleResult(Action action)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                action();
+                return NoContent();
+            }
+            catch (HttpException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.ErrorMessage);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
         /// <summary>
         /// Handles the result of an HTTP action.
         /// </summary>
@@ -14,7 +44,7 @@ namespace ShuttleZone.Api.Controllers.BaseControllers
         /// If the model state is not valid, the method will return a bad request with the model state.
         /// If the action throws an exception, the method will return a status code of 500 with the exception message.
         /// </remarks>
-        protected IActionResult HandleResult(Func<object> action)
+        protected IActionResult HandleResult<T>(Func<T> action)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -23,6 +53,10 @@ namespace ShuttleZone.Api.Controllers.BaseControllers
             {
                 var result = action();
                 return Ok(result);
+            }
+            catch (HttpException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.ErrorMessage);
             }
             catch (Exception ex)
             {
@@ -39,7 +73,7 @@ namespace ShuttleZone.Api.Controllers.BaseControllers
         /// If the model state is not valid, the method will return a bad request with the model state.
         /// If the action throws an exception, the method will return a status code of 500 with the exception message.
         /// </remarks>
-        protected async Task<IActionResult> HandleResultAsync(Func<Task<object>> action)
+        protected async Task<IActionResult> HandleResultAsync<T>(Func<Task<T>> action)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -50,6 +84,39 @@ namespace ShuttleZone.Api.Controllers.BaseControllers
                 if (result == null)
                     return NotFound();
                 return Ok(result);
+            }
+            catch (HttpException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.ErrorMessage);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Handles the result of an HTTP action asynchronously without a return value.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>The result of the action.</returns>
+        /// <remarks>
+        /// If the model state is not valid, the method will return a bad request with the model state.
+        /// If the action throws an exception, the method will return a status code of 500 with the exception message.
+        /// </remarks>
+        protected async Task<IActionResult> HandleResultAsync(Func<Task> action)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                await action();
+                return NoContent();
+            }
+            catch (HttpException ex)
+            {
+                return StatusCode(ex.StatusCode, ex.ErrorMessage);
             }
             catch (Exception ex)
             {
--- src/ShuttleZone.Application/Services/Contest/ContestService.cs
+++ src/ShuttleZone.Application/Services/Contest/ContestService.cs
@@ -1,10 +1,13 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using ShuttleZone.Application.Common.Interfaces;
 using ShuttleZone.Common.Attributes;
+using ShuttleZone.Common.Exceptions;
 using ShuttleZone.DAL.Common.Interfaces;
 using ShuttleZone.DAL.Repositories;
 using ShuttleZone.Domain.Entities;
+using ShuttleZone.Domain.Enums;
 using ShuttleZone.Domain.WebRequests;
 using ShuttleZone.Domain.WebResponses.Contest;
 
@@ -16,12 +19,14 @@ public class ContestService : IContestService
     private readonly IContestRepository _contestRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUser _currentUser;
 
-    public ContestService(IContestRepository contestRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    public ContestService(IContestRepository contestRepository, IMapper mapper, IUnitOfWork unitOfWork, IUser currentUser)
     {
         _contestRepository = contestRepository;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _currentUser = currentUser;
     }
 
     public IQueryable<DtoContestResponse> GetContests()
@@ -59,7 +64,26 @@ public class ContestService : IContestService
 
     public async Task<DtoContestResponse> CreateContestAsync(CreateContestRequest request, CancellationToken cancellationToken)
     {
+        HttpException.New()
+            .WithStatusCode(401)
+            .WithErrorMessage("You are not authorized to create a contest")
+            .ThrowIfNull(_currentUser.Id)
+            .ThrowIf(!Guid.TryParse(_currentUser.Id, out var userIdAsGuid));
+
+        var contestCreator = new UserContest
+        {
+            ParticipantsId = userIdAsGuid,
+            isCreatedPerson = true,
+            isWinner = false,
+            Point = 0
+        };
         var contest = _mapper.Map<Contest>(request);
+        contest.MaxPlayer = 2;
+        contest.ContestStatus = ContestStatusEnum.Open;
+        contest.CreatedBy = _currentUser.Id;
+        contest.Created = DateTime.UtcNow;
+        contest.LastModified = DateTime.UtcNow;
+        contest.UserContests.Add(contestCreator);
 
         await _contestRepository.AddAsync(contest, cancellationToken);
         await _unitOfWork.CompleteAsync(cancellationToken);
