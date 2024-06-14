237d1d4a68dcfd91c83413266ce28f20b6c998cd
viet-quanggg <nguyenvietquang099@gmail.com>
2024-06-14 14:00:28 +0700

    minot fix for authen

--- src/ShuttleZone.Api/Controllers/ReservationDetailsController.cs
+++ src/ShuttleZone.Api/Controllers/ReservationDetailsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
 using ShuttleZone.Api.Controllers.BaseControllers;
+using ShuttleZone.Application.Common.Interfaces;
 using ShuttleZone.Application.Services.Reservation;
 using ShuttleZone.Application.Services.Token;
 
@@ -10,22 +11,25 @@ namespace ShuttleZone.Api.Controllers
     {
         private readonly IReservationService _reservationService;
         private readonly ITokenService _tokenService;
+        private readonly IUser _user;
         
 
-        public ReservationDetailsController(IReservationService reservationService, ITokenService tokenService)
+        public ReservationDetailsController(IReservationService reservationService, ITokenService tokenService, IUser user)
         {
             _reservationService = reservationService;
             _tokenService = tokenService;
+            _user = user;
         }
 
         [EnableQuery]
         public IActionResult Get()
         {
             //placeholder for logined user
-            var userId = new Guid("26A7CC4E-3F9B-4923-809E-2F9B771D994F");
-            var authModel = _tokenService.GetAuthModel(GetJwtToken());
+            // var userId = new Guid("26A7CC4E-3F9B-4923-809E-2F9B771D994F");
+            // var authModel = _tokenService.GetAuthModel(GetJwtToken());
+            var userId = new Guid(_user.Id?? throw new ArgumentNullException());
 
-            var response = _reservationService.GetMyReservationDetails(authModel.UserId);
+            var response = _reservationService.GetMyReservationDetails(userId);
 
             return Ok(response);
         }
