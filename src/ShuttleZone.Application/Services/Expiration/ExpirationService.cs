09ae76969e4fd4fb2cc2521151a2842e10b9680f
supernhilty <nhilty302814@gmail.com>
2024-07-15 18:28:40 +0700

    [ExpiredPackage][Nhi] chore: Update ExpirationService to send notification when package is about to expire

--- src/ShuttleZone.Application/Services/Expiration/ExpirationService.cs
+++ src/ShuttleZone.Application/Services/Expiration/ExpirationService.cs
@@ -1,28 +1,27 @@
 ﻿using System.Globalization;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using ShuttleZone.Application.Services.Email;
+using ShuttleZone.Application.Services.Notifications;
 using ShuttleZone.Common.Attributes;
+using ShuttleZone.Common.Exceptions;
 using ShuttleZone.DAL.Common.Interfaces;
 using ShuttleZone.DAL.DependencyInjection.Repositories.PackageUser;
 using ShuttleZone.DAL.Repositories;
+using ShuttleZone.Domain.Entities;
 using ShuttleZone.Domain.Enums;
+using ShuttleZone.Domain.WebRequests.Notifications;
+using ShuttleZone.Domain.WebResponses.Notifications;
 
 namespace ShuttleZone.Application.Services.Expiration;
 [AutoRegister]
-public class ExpirationService : IExpirationService
-{
-    private readonly IPackageUserRepository _packageUserRepository;
-    private readonly IClubRepository _clubRepository;
-    private readonly IEmailService _emailService;
-    private readonly IUnitOfWork _unitOfWork;
-
-    public ExpirationService(IPackageUserRepository packageUserRepository, IUnitOfWork unitOfWork, IEmailService emailService, IClubRepository clubRepository)
-    {
-        _packageUserRepository = packageUserRepository;
-        _unitOfWork = unitOfWork;
-        _emailService = emailService;
-        _clubRepository = clubRepository;
-    }
+public class ExpirationService(IPackageUserRepository _packageUserRepository, 
+    IUnitOfWork _unitOfWork, 
+    IEmailService _emailService, 
+    IClubRepository _clubRepository,
+    INotificationHubService _notificationHubService,
+    IMapper _mapper) : IExpirationService
+{ 
     public async Task ValidatePackageUser()
     {
         try
@@ -44,12 +43,22 @@ public class ExpirationService : IExpirationService
                     ExpirationDay = packageUser.EndDate.ToString(CultureInfo.InvariantCulture),
                     PackageName = packageUser.Package.Name,
                     RenewPackageUrl = "http://localhost:3000/home"
-                };
+                };                
 
                 var remainingDays = (packageUser.EndDate.Date - DateTime.Now.Date).Days;
                 emailObject.RemainingDays = remainingDays;
                 await _emailService.SendExpirationPackageEmail(emailObject);
-                
+
+                //noti for user
+                var notificationRequest = new NotificationRequest
+                {
+                    UserId = packageUser.UserId,
+                    Description = $"Gói đăng ký câu lạc bộ của bạn hết hạn trong {remainingDays}. Hãy đăng ký gói lại đúng thời gian để câu lạc bộ hoạt động nhé.",
+                };
+                var notification = _notificationHubService.CreateNotification(notificationRequest);
+                await _unitOfWork.NotificationRepository.AddAsync(notification);
+                await _notificationHubService.SendNotificationAsync((Guid)packageUser.UserId, _mapper.Map<NotificationResponse>(notification));
+
                 if (remainingDays != 0) continue;
                 packageUser.PackageUserStatus = PackageUserStatus.EXPIRED;
                 _packageUserRepository.Update(packageUser);
